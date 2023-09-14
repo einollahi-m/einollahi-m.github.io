c520e910eb948aa253070fad069366f72f440f2f
Mohammad-Reza EInollahi <einollahimr@gmail.com>
2023-09-14 10:11:41 +0330

    New web-site
    My web-page using the Vue.js. First sketch.

---
+++ assets/Blog-33f3a02e.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as o}from"./index-4e95ce5e.js";const n={};function r(t,s){return c(),o("h2",null,"Blog")}const a=e(n,[["render",r]]);export{a as default};
---
+++ assets/Resume-9c8a44b1.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as n}from"./index-4e95ce5e.js";const o={};function r(s,t){return c(),n("h2",null,"Resume")}const a=e(o,[["render",r]]);export{a as default};
