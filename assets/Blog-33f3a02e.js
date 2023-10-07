aa6baa49f3eb3925a287159cb139bcbbbc4c2b08
Mohammad-Reza EInollahi <einollahimr@gmail.com>
2023-10-07 16:27:17 +0330

    Update

--- assets/Blog-33f3a02e.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as o}from"./index-4e95ce5e.js";const n={};function r(t,s){return c(),o("h2",null,"Blog")}const a=e(n,[["render",r]]);export{a as default};
--- assets/Research-2e4cc3e5.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as r}from"./index-4e95ce5e.js";const n={};function o(s,t){return c(),r("h2",null,"Research")}const _=e(n,[["render",o]]);export{_ as default};
